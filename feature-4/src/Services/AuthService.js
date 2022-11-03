900bd15b3b54d677d13c5baca4decbb7c048014b
Aidan Gordon <agordon611@gmail.com>
2022-11-03 16:32:57 -0400

    AuthService Added

---
+++ feature-4/src/Services/AuthService.js
@@ -0,0 +1,19 @@
+import Parse from "parse";
+
+// used in auth register component
+export const createUser = (newUser) => {
+  const user = new Parse.User();
+
+  user.set("username", newUser.email);
+  user.set("password", newUser.password);
+
+  console.log("User: ", user);
+  return user
+    .signUp()
+    .then((newUserSaved) => {
+      return newUserSaved;
+    })
+    .catch((error) => {
+      alert(`Error: ${error.message}`);
+    });
+};
