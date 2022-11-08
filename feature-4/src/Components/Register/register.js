b1948f90ffd731bff06d58b125dbbfd1f94fa893
Aidan Gordon <agordon611@gmail.com>
2022-11-08 16:30:26 -0500

    Added Functionality to Register

--- feature-4/src/Components/Register/register.js
+++ feature-4/src/Components/Register/register.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import { checkUser, createUser } from "../../Services/AuthService";
 
 
 const Register = () => {
+    const navigate = useNavigate();
+
+    const [newUser, setNewUser] = useState({
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: ""
+    });
+
+    // flags in the state to watch for add/remove updates
+    const [add, setAdd] = useState(false);
+
+    // redirect already authenticated users back to home
+    useEffect(() => {
+        if (checkUser()) {
+        alert("You are already logged in");
+        navigate("/");
+        }
+    }, [navigate]);
+    
     return (
         <div>This will be the regsiter page</div>
     );
