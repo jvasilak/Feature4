1ae5cee2434e54f03b5184bffbd978bec598162e
jvasilak <jvasilak@nd.edu>
2022-11-09 18:39:57 -0500

    fix register form

--- feature-4/src/Components/Register/register.js
+++ feature-4/src/Components/Register/register.js
@@ -1,7 +1,7 @@
-import React, {useState, useNavigate, useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import { checkUser, createUser } from "../../Services/AuthService";
 import AuthRegister from './AuthRegister';
-import {Link} from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
 
 const Register = () => {
     const navigate = useNavigate();
@@ -96,7 +96,6 @@ const Register = () => {
                 </div>
             </form>
             <p className="registerLink">Already have an account? <Link to="/login">Click Here to Login</Link></p>
-            < AuthRegister />
         </div>
     );
 }
