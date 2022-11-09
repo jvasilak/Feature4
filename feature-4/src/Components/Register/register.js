f0bd5349978aeb55cdb2c83c304e4170e4aa141c
jvasilak <71355888+jvasilak@users.noreply.github.com>
2022-11-08 22:50:57 -0500

    Update register.js

--- feature-4/src/Components/Register/register.js
+++ feature-4/src/Components/Register/register.js
@@ -59,7 +59,44 @@ const Register = () => {
       };
 
     return (
-        < AuthRegister />
+        <div>
+            <h1 className="pageHeader">Sign Up</h1>
+            <form method="GET" onSubmit={(event) => {
+                createAccount();
+            }}>
+                <div className="loginInput">
+                <label>Username</label>
+                <input type="text"
+                    onChange={(event) => setNewUser(event.target.value)} />
+                </div>
+                <div className="loginInput">
+                <label>Email</label>
+                <input type="email"
+                    onChange={(event) => setNewUser(event.target.value)} />
+                </div>
+                <div className="loginInput">
+                <label>First Name</label>
+                <input type="text"
+                    onChange={(event) => setNewUser(event.target.value)} />
+                </div>
+                <div className="loginInput">
+                <label>Last Name</label>
+                <input type="text"
+                    onChange={(event) => setNewUser(event.target.value)} />
+                </div>
+                <div className="loginInput">
+                <label>Password</label>
+                <input type="password"
+                    onChange={(event) => setNewUser(event.target.value)} />
+                <br />
+                </div>
+                <div className="loginInput">
+                <input className="loginSubmit" type="submit" value="Create Account" />
+                </div>
+            </form>
+            <p className="registerLink">Already have an account? <Link to="/login">Click Here to Login</Link></p>
+            < AuthRegister />
+        </div>
     );
 }
 
