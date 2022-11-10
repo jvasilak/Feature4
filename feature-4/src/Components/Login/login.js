a17f40e734821057fafe71df9403e608ee121129
jvasilak <jvasilak@nd.edu>
2022-11-09 20:45:47 -0500

    add login function

--- feature-4/src/Components/Login/login.js
+++ feature-4/src/Components/Login/login.js
@@ -2,52 +2,85 @@ import React, {
     useState,
     useEffect
   } from "react";  
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { getAllUsers } from "../../Services/users";
 import './login.css';
+import { loginUser } from "../../Services/AuthService";
 
   const LoginPage = (props) => {
+    const navigate = useNavigate();
     const [users, setUsers] = useState([]);
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
+    const [add, setAdd] = useState(false);
+    const [loginUserInfo, setLoginUser] = useState({
+      username: "",
+      password: ""
+  });
     useEffect(() => {
       getAllUsers().then((users) => {
         setUsers(users);
       });
     }, []);
     // We currently do not have many users created, in order to see the effects of a successful login try "admin" for both the username and password
-    const attemptLogin = (submitEvent) => {
-      users.map((user) => {
-        if(user.get("Username") === username) {
-          if(user.get("Password") === password) {
-            props.changeState(0);
-            props.changeLoginStatus(true);
-            alert("success");
-            return;
+    useEffect(() => {
+      // checkUser() ? history.push("/home"): null;
+      if (loginUserInfo && add) {
+        loginUser(loginUser).then((userLoggedIn) => {
+          if (userLoggedIn) {
+            alert(
+              `${userLoggedIn.get("firstName")}, you successfully logged in!`
+            );
+            props.updateLoginStatus(true);
+            navigate("/dashboard");
           }
-        }
+          // TODO: redirect user to main app
+          setAdd(false);
+        });
+      }
+    }, [navigate, loginUserInfo, add]);
+
+    const onSubmitHandler = (e) => {
+      e.preventDefault();
+      console.log("submitted: ", e.target);
+      setAdd(true);
+    };
+
+    const onChangeHandler = (e) => {
+      e.preventDefault();
+      console.log(e.target);
+      const { name, value: newValue } = e.target;
+      console.log(newValue);
+  
+      setLoginUser({
+        ...loginUserInfo,
+        [name]: newValue
       });
-      
-      submitEvent.preventDefault();
     };
   
     if (users.length > 0) {
       return (<div>
         <h1 className="pageHeader">Login</h1>
         <form method="GET" onSubmit={(event) => {
-          attemptLogin(event);
+          onSubmitHandler(event);
 
         }}>
           <div className="loginInput">
 
             <label>Username</label>
             <input type="text"
-              onChange={(event) => setUsername(event.target.value)} />
+              name="username"
+              value={loginUserInfo.username}
+              placeholder="Username"
+              required
+              onChange={(event) => onChangeHandler(event)} />
           </div>
           <div className="loginInput">
             <label>Password</label>
             <input type="password"
-              onChange={(event) => setPassword(event.target.value)} />
+              value={loginUserInfo.password}
+              name="password"
+              placeholder="Password"
+              required
+              onChange={(event) => onChangeHandler(event)} />
             <br />
           </div>
           <div className="loginInput">
