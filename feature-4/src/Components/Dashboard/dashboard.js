b2988c50d0bfe6f87c4efe723eafb154b48ee66a
Aidan Gordon <agordon611@gmail.com>
2022-11-15 16:06:56 -0500

    Fixed Login Alert

--- feature-4/src/Components/Dashboard/dashboard.js
+++ feature-4/src/Components/Dashboard/dashboard.js
@@ -1,30 +1,33 @@
 import React from 'react';
 import './dashboard.css';
 import Parse from "parse";
-
+import AdminDashboard from "./adminDashboard.js";
 
 const Dashboard = (props) => {
     const user = Parse.User.current();
-    return (<div>
-        <h1 className='pageHeader'>Welcome <i>{user.get("username")}</i></h1>
-        <div className='sectionHeader'>
-            <h2>Teams</h2>
-            <ul>
-                <li>
-                    Your teams will go here
-                </li>
-            </ul>
-        </div>
-        <div className='sectionHeader'>
-            <h2>Games</h2>
-            <ul>
-                <li>
-                    Your upcoming games will go here
-                </li>
-            </ul>
-        </div>
-    </div>);
-    
+    if (user.get("admin")) {
+        return (<AdminDashboard />);
+    } else {
+        return (<div>
+            <h1 className='pageHeader'>Welcome <i>{user.get("username")}</i></h1>
+            <div className='sectionHeader'>
+                <h2>Teams</h2>
+                <ul>
+                    <li>
+                        Your teams will go here
+                    </li>
+                </ul>
+            </div>
+            <div className='sectionHeader'>
+                <h2>Games</h2>
+                <ul>
+                    <li>
+                        Your upcoming games will go here
+                    </li>
+                </ul>
+            </div>
+        </div>);
+    }
 }
 
 export default Dashboard;
