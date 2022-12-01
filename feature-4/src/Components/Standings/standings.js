4c33d7632b0bfde1fe3d3531e5eceb09ac63deff
jvasilak <jvasilak@nd.edu>
2022-12-01 15:28:57 -0500

    pre-review push

--- feature-4/src/Components/Standings/standings.js
+++ feature-4/src/Components/Standings/standings.js
@@ -1,17 +1,40 @@
 import React, {useEffect, useState} from 'react';
 import { getAllTeams } from '../../Services/teams';
 
+// win percent, head to head, goal difference
+const sortStandings = () => {
+
+}
+
 const Standings = (props) => {
-    const [teams, setTeams] = useState([]);
+    const [allTeams, setAllTeams] = useState([]);
     useEffect(() => {
         getAllTeams().then((team) => {
-        setTeams(team);
+        setAllTeams(team);
         })
     }, []);
+    const [teams, setTeams] = useState([]);
+    useEffect(() => {
+        setTeams(allTeams.filter(function (team) {
+            return props.teamIDs.includes(team.get("ID"))
+        }));
+    }, [allTeams]);
+    let [sortedTeams, setSortedTeams] = useState([]);
+    useEffect(() => {
+        setSortedTeams(
+            Object.entries(teams).sort((a,b) => a["WinPct"] > b["WinPct"])
+        )
+    }, [teams]);
+    console.log(teams);
+    console.log(sortedTeams);
     return(
         <div>
             <ul>
                 <li>Team | GP | W | L | T | PCT</li>
+                {teams.map((team) => {
+                    return(<li>{team.get("Name")} {team.get("GamesPlayed")} {team.get("Wins")} 
+                    {" "}{team.get("Losses")} {team.get("Ties")} {team.get("WinPct")}</li>);
+                })}
             </ul>
         </div>
     )
